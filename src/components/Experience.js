ef24048acf8e9eecc9ce08df3f5327bd43d82de8
unknown <alisalovescode@gmail.com>
2023-02-01 14:03:10 -0500

    added Experience component (still wip)

---
+++ src/components/Experience.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Experience = () => {
+    return (
+        <div className="experience-content">
+            
+        </div>
+    )
+}
+
+export default Experience
